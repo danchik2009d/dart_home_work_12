ff0f05520c34dc3a1e3987de76211395dc157626
danchik2009d <danielsabatbekova@gmail.com>
2024-12-04 18:17:33 +0300

    first commit

---
+++ dart_switch_cases_homework.dart
@@ -0,0 +1,71 @@
+
+
+
+import 'dart:io';
+void main(List<String> arguments) {
+/*  print('напишите число от 1 до 31');
+int? day = int.tryParse(stdin.readLineSync()??'');
+if (day == null || day > 31) {
+   print('неправильно,напишите число \n в диапозоне от 1 до 31');
+  return;  }
+
+switch (day){
+  case 1: case 2: case 3: case 4: case 5:case 6:
+  case 7:case 8:case 9:case 10:
+
+print('ваше число в первой части');
+break;
+ case 11: case 12:case 13:case 14:case 15:case 16:
+ case 17:case 18:case 19:case 20:
+ print('ваше число во второй части ');
+ break;
+ default:
+ print('ваше число в третьей части');
+*/
+
+
+// Задание 2 (через терминал, для проверки нужно убрать коммент)
+
+
+
+/*print('напишите какое число от 1 до 12');
+int? month = int.tryParse(stdin.readLineSync()??'');
+if (month == null || month > 12) {
+  print('неправильно введите число \n в диапозоне от 1 до 12');
+  return;
+  }
+  
+  switch(month){
+    case 1: case 2:   case 3: 
+    print('осень');
+    break;
+    case 4 : case 5: case 6:
+    print('зима');
+    break;
+    case 7: case 8: case 9:
+    print('весна');
+    break;
+    case 10: case 11: case 12:
+    print('лето');
+    break;
+    default:
+    print('ERROR');
+  
+  }
+*/
+
+
+// задание 3
+String b = 'abcde';
+String c = b[0];
+if (c == 'a'){
+  print('да');
+  
+}
+else {
+print('нет');
+}
+
+}
+
+
